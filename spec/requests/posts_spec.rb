51e73fd56a3bb9f7be94b886eb26da18b3240a62
GhislainMitahi <ghislainmitahi@gmail.com>
2023-05-11 13:22:24 +0200

    test controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+describe 'Posts', type: :request do
+  let!(:user) do
+    User.create!(
+      name: Faker::Name.name,
+      bio: Faker::Lorem.sentence,
+      photo: Faker::Avatar.image
+    )
+  end
+  let!(:post) do
+    Post.create!(
+      title: Faker::Lorem.sentence(word_count: 3),
+      text: Faker::Lorem.paragraph,
+      author: user
+    )
+  end
+
+  describe 'GET #index' do
+    it 'returns http success' do
+      get user_posts_path(user.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get user_posts_path(user.id)
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get user_posts_path(user.id)
+      expect(response.body).to include(user.name)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get user_post_path(user.id, post.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get user_post_path(user.id, post.id)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get user_post_path(user.id, post.id)
+      expect(response.body).to include(post.title)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+describe 'Users', type: :request do
+  let!(:user) do
+    User.create!(
+      name: Faker::Name.name,
+      bio: Faker::Lorem.sentence,
+      photo: Faker::Avatar.image
+    )
+  end
+
+  describe 'GET #index' do
+    it 'returns http success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('All Users')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get user_path(user.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get user_path(user.id)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get user_path(user.id)
+      expect(response.body).to include(user.name)
+    end
   end
 end
