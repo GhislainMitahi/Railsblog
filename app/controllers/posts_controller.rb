9239a855660d9f69aed2e2bd68ce07abb3a4669f
GhislainMitahi <ghislainmitahi@gmail.com>
2023-05-13 11:55:42 +0200

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,37 @@
 class PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = Post.all
-    end
-  
-    def show
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:id])
-      @comment = Comment.new
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = Post.new(
-        title: params[:post][:title],
-        text: params[:post][:text],
-        author: current_user
-      )
-  
-      if @post.save
-        redirect_to user_post_path(@post.author, @post)
-      else
-        flash[:error] = 'Post could not be saved'
-        redirect_to user_posts_path(@post.author)
-      end
-    end
-  
-    private
-  
-    def post_params
-      params.require(:post).permit(:title, :text)
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.all
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title: params[:post][:title],
+      text: params[:post][:text],
+      author: current_user
+    )
+
+    if @post.save
+      redirect_to user_post_path(@post.author, @post)
+    else
+      flash[:error] = 'Post could not be saved'
+      redirect_to user_posts_path(@post.author)
     end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
