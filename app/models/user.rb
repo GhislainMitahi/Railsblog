771cbfe9a50214a062a8596d91980d13dc0baf75
biruk-bereka <birukbeletech1212@gmail.com>
2023-05-19 11:47:24 +0300

    Fix path error and update files

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def likes?(post)
-    likes.exists?(post)
+    likes.exists?(post.id)
   end
 
   def like_for(post)
-    likes.find_by(post)
+    likes.find_by(post_id: post.id)
   end
 
   def recent_posts(limit = 3)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Create 10 users with randomized data
-10.times do
+2.times do
     User.create!(
       name: Faker::Name.name,
       photo: Faker::Avatar.image,
@@ -17,7 +17,7 @@
   end
   
   # Create 100 comments with randomized data
-  100.times do
+  5.times do
     Comment.create!(
       text: Faker::Lorem.paragraph(sentence_count: 10),
       author: User.order(Arel.sql('RANDOM()')).first,
@@ -26,7 +26,7 @@
   end
   
   # Create 200 likes with randomized data
-  500.times do
+  10.times do
     user = User.order(Arel.sql('RANDOM()')).first
     post = Post.order(Arel.sql('RANDOM()')).first
     # But only if the user hasn't liked the post yet
